1cbbe5167685342e3f5d9dee71979e69fd488fd5
Praharsh-pai <dementor1901@gmail.com>
2022-09-30 15:25:12 +0530

    Praharsh-pai-nextJS

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import { useEffect } from 'react';
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    require('bootstrap/dist/js/bootstrap');
-  }, []);
   return <Component {...pageProps} />
 }
 
