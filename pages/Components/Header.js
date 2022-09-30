f74b390593e12ec886afe9c133e3f26e43f7ddc1
Praharsh-pai <dementor1901@gmail.com>
2022-09-30 15:19:24 +0530

    Praharsh-pai-nextJS

--- pages/Components/Header.js
+++ pages/Components/Header.js
@@ -9,18 +9,17 @@ const Header = () => {
             <Link href="/"><a><Image height='50' width='50' src="https://abhiganesh.com/assets/nav/Abhiganesh-Logo.svg" alt="Kapslock logo"/></a></Link>
         </div>
         
-        <ul >{/*class="nav-list"*/}
+        <ul>{/*class="nav-list"*/}
             <li><Link href="/"><a>Home</a></Link>{/*class="nav-item"*/}</li>
             <li><Link href="/Aboutus"><a>About Us</a></Link>{/*class="nav-item"*/}</li>
             <li >{/*class="nav-dropdown-header"*/}
-                <a>Solutions<Image height='50' width='50' class="dropdown-image" src="https://abhiganesh.com/assets/nav/dropdown.svg" alt="dropdown icon"/></a>{/*class="nav-item"*/}
-                <ul class="nav-dropdown display-toggle">{/**/}
+                <a>Solutions<Image height='50' width='50'  src="https://abhiganesh.com/assets/nav/dropdown.svg" alt="dropdown icon"/>{/*class="dropdown-image"*/} </a>{/*class="nav-item"*/}
+                <ul >{/*class="nav-dropdown display-toggle"*/}
                     <li><Link href="/preservingsolutions"><a>Preserving</a></Link></li>
                     <li><Link href="/financingsolutions"><a>Financing</a></Link></li>
                     <li><Link href="/investingsolutions"><a>Investing</a></Link></li>
                 </ul>
             </li>
-
             <li><Link href="https://wealthelite.in/client-login"><a>Client-Login</a></Link>{/*class="nav-item"*/}</li>
             <li><Link href="/Contact"><a>Contact Us</a></Link>{/*class="nav-cta cta-primary"*/}</li>
         </ul>
