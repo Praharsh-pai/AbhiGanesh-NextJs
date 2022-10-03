9872a83dcaa4ec4e78b67d66e0029d604f9d24e5
Praharsh-pai <dementor1901@gmail.com>
2022-10-04 02:46:41 +0530

    add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e7ea99c1df68a7b"; }
+/******/ 		__webpack_require__.h = function() { return "4336fef41e3fca9c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
